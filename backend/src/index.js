775cdaa31e8425ad0327a5019a5b6e44a0fe72dc
Maitê Caetano <maite@MacBook-Air-de-Maite.local>
2020-05-07 18:40:19 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const cors = require('/cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors()); // perimite que as aplicações front end 
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333); 
